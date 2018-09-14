79dd5fba98bc1c1469e05247a7acdd9206efacf7
Lucas Phan <devmachine2@DevMachine2s-MacBook-Pro.local>
2018-09-14 13:07:08 -0700

    exam start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,17 +7,11 @@ import { FlexModalService } from './shared-components/flex-modal/flex-modal.serv
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'final';
-  aboutText: string;
   constructor(private flexModal: FlexModalService) { }
 
   ngOnInit() {
-    this.aboutText = 'This application is developed by (C) Lucas Phan';
   }
 
   // Launch flex modal or alert (3pts reduced) showing about text
-  openAboutModal() {
-    this.flexModal.openDialog('about-modal');
-  }
 
 }
