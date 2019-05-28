22b45466c23caf0c75283a07e493ee539a138490
Kong Moua <kongmoua97@mail.fresnostate.edu>
2019-05-27 18:56:52 -0700

    finish exam

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FlexModalService } from './shared-components/flex-modal/flex-modal.service';
+import { Router } from '@angular/router';
+import { OrdersComponent } from './orders/orders.component';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +9,18 @@ import { FlexModalService } from './shared-components/flex-modal/flex-modal.serv
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  constructor(private flexModal: FlexModalService) { }
+  aboutText = 'Created by Kong Moua 2019';
+
+  constructor(private flexModal: FlexModalService, private router: Router) { }
 
   ngOnInit() {
   }
 
   // Launch flex modal or alert (3pts reduced) showing about text
+  showHelpText() {
+  this.flexModal.openDialog('about-modal');
+  }
 
-}
+  navigateTo(path: string) {
+    this.router.navigate(['orders']);
+  }
--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -11,7 +11,8 @@ import { Http } from '@angular/http';
 })
 
 export class OrdersComponent implements OnInit {
-  orders;
+  orders: Array<any> = [];
+
   constructor(
     private router: Router,
     private flexModal: FlexModalService,
@@ -21,8 +22,10 @@ export class OrdersComponent implements OnInit {
   }
 
   async ngOnInit() {
-
+    this.loadSavedOrders();
   }
+
+
 
   // Calculate total and perform input validation
 
@@ -33,5 +36,28 @@ export class OrdersComponent implements OnInit {
   // delete line item (order) when delete button is click
 
   // read in the orders.json file and populate the list table with the initial orders (3)
+  loadSavedOrders() {
+    this.savedOr = [{
+      'pid': 'P414',
+      'description': '1/4 Pipe',
+      'price': 12.00,
+      'inventory': 30,
+      'quantity': 2
+    }, {
+      'pid': 'T208',
+      'description': '3/8 T Joint',
+      'price': 64.00,
+      'inventory': 74,
+      'quantity': 0
+    }, {
+      'pid': 'B101',
+      'description': 'Break Disk A',
+      'price': 85.00,
+      'inventory': 98,
+      'quantity': 2
+    }];
 
+  }
+  clearOrders() {
+  }
 }
